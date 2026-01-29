39161934206ce976d597c80252c6550aa0c0154a
Ghivarra Senandika R <gsenandika@gmail.com>
2026-01-29 10:43:10 +0700

    Build files

---
+++ dist/src/StyleProviderContext.js
@@ -0,0 +1,4 @@
+Object.defineProperty(exports,"__esModule",{value:true});exports.StyleContext=undefined;var _react=require('react');var _react2=_interopRequireDefault(_react);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}
+
+var StyleContext=exports.StyleContext=_react2.default.createContext(null);
+//# sourceMappingURL=StyleProviderContext.js.map
